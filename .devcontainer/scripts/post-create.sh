844ed96bddc405d78b6ffd5f4c2c1f5aa50ac00f
T448 <tatsuda4480712@gmail.com>
2025-10-11 08:53:44 +0000

    [update]エイリアス追加とccのバージョンアップ #3

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -3,10 +3,20 @@ sudo apt-get install -y tig procps
 sudo apt-get clean
 rm -rf /var/lib/apt/lists/*
 
+# エイリアスなどの設定
+{
+    echo 'alias ll="ls -la --color=auto"'
+    echo 'function chpwd() {
+    pwd
+    echo "-------------------------"
+    ls
+}'
+} >> ~/.zshrc
+
 pip install --no-cache-dir uv==0.8.22
 
 # claude-code, gemini-cli, lefthook をインストール
-npm install -g @anthropic-ai/claude-code@2.0.5 @google/gemini-cli@0.6.1 lefthook@1.13.4
+npm install -g @anthropic-ai/claude-code@2.0.13 @google/gemini-cli@0.8.2 lefthook@1.13.4
 
 # CCPluginsをインストール
 curl -sSL https://raw.githubusercontent.com/brennercruvinel/CCPlugins/main/install.sh | bash
